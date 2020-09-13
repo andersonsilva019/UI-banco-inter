e3c02655f406ae891ef23b6286ebaf71ff2dc53a
Anderson0017 <andeerson0017@gmail.com>
2020-09-13 15:40:52 -0300

    :lipstrick: feat: add styles and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-function App() {
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
+
+const App: React.FC = () => {
   return (
-    <h1>
-      teste dsfosd sdfsdifjsdf sdfosdfosdjfjdsf dsfodsjfosdjfodsf dsfksdofsdofjs
-    </h1>
+    <ThemeProvider theme={theme}>
+      <h1>Hello</h1>
+      <GlobalStyles />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
