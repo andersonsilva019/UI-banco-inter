c7ae25040da191ef15b4dd26e4c4e0a1075b751b
Anderson0017 <andeerson0017@gmail.com>
2020-09-13 21:06:17 -0300

    :truck: fix: move gradient component from component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 import Header from './components/Header';
-import Gradient from './components/Header/Gradient';
+import Gradient from './components/Gradient';
 
 const App: React.FC = () => {
   return (
