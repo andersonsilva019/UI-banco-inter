c1bf8ae17d3387c4223e69916ed5a1d8b9df17c3
andersonsilva019 <andeerson0017@gmail.com>
2020-10-02 09:59:01 -0300

    :sparkles: feat: add line chart do account summary

--- src/pages/Dashboard/MainContent/AccountSummary/index.tsx
+++ src/pages/Dashboard/MainContent/AccountSummary/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 
 import { FiFileText, FiCreditCard, FiEye, FiEyeOff } from 'react-icons/fi';
+import { ResponsiveLine } from '@nivo/line';
+import { useTheme } from 'styled-components';
 
 import { PlataformaPaiIcon } from '../../../../assets/icons';
 import { IlustratorImage } from '../../../../assets/images';
+import { dataLine } from '../../../../resources';
 import {
   Container,
   Card,
@@ -12,12 +15,24 @@ import {
   LeftData,
   RightData,
   DataValue,
+  CustomTooltip,
 } from './styles';
 import Button from '../../../../components/Button';
+
+type ChartValue = number | React.ReactText;
+
+const formatChartData = (number: ChartValue): string => `${number || 0}%`;
 
 const AccountSummary: React.FC = () => {
   const [displayStatement, setDisplayStatement] = useState(true);
   const [displayInvestments, setDisplayInvestments] = useState(true);
+  const [investmentsGrowth, setInvestmentsGrowth] = useState(() => {
+    const [investment] = dataLine;
+    const { y } = investment.data[investment.data.length - 1];
+    return formatChartData(y);
+  });
+
+  const { colors } = useTheme();
 
   return (
     <Container>
@@ -33,7 +48,7 @@ const AccountSummary: React.FC = () => {
           </Button>
         </Header>
         <DataWrapper>
-          <LeftData>Grafico</LeftData>
+          <LeftData>Greafico</LeftData>
           <RightData>
             <span>Receita</span>
             <DataValue income>
@@ -72,7 +87,48 @@ const AccountSummary: React.FC = () => {
           </Button>
         </Header>
         <DataWrapper>
-          <LeftData>Grafico</LeftData>
+          <LeftData>
+            <ResponsiveLine
+              data={dataLine}
+              enableArea
+              enableGridY={false}
+              curve="cardinal"
+              margin={{ top: 8, right: 8, bottom: 20, left: 8 }}
+              xScale={{ type: 'point' }}
+              yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: true,
+                reverse: false,
+              }}
+              tooltip={({ point }) => {
+                return (
+                  <CustomTooltip>
+                    {formatChartData(point.data.yFormatted)}
+                  </CustomTooltip>
+                );
+              }}
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                orient: 'bottom',
+                tickSize: 0,
+                tickPadding: 5,
+                tickRotation: 0,
+              }}
+              axisLeft={null}
+              colors={colors.success}
+              enableCrosshair={false}
+              pointSize={8}
+              lineWidth={1.5}
+              pointColor={colors.success}
+              pointBorderColor={{ from: 'serieColor' }}
+              pointLabel="y"
+              pointLabelYOffset={-12}
+              useMesh
+            />
+          </LeftData>
           <RightData>
             <span>Total investido</span>
             <DataValue income>
@@ -80,7 +136,7 @@ const AccountSummary: React.FC = () => {
             </DataValue>
             <span>Evolução no mês</span>
             <DataValue outcome>
-              {displayInvestments ? 'R$ 0,00' : '---'}
+              {displayInvestments ? investmentsGrowth : '---'}
             </DataValue>
           </RightData>
         </DataWrapper>
---
+++ src/resources/data-bump-graph.tsx
@@ -0,0 +1,34 @@
+import theme from '../styles/theme';
+
+export default [
+  {
+    id: 'Investments',
+    color: theme.colors.success,
+    data: [
+      {
+        x: 'nov',
+        y: 2,
+      },
+      {
+        x: 'dez',
+        y: 19,
+      },
+      {
+        x: 'jan',
+        y: 18,
+      },
+      {
+        x: 'fev',
+        y: 15,
+      },
+      {
+        x: 'mar',
+        y: 20,
+      },
+      {
+        x: 'abr',
+        y: 30,
+      },
+    ],
+  },
+];
