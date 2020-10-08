54fcf76622f97529620df60986397d1e89c3336e
andersonsilva019 <andeerson0017@gmail.com>
2020-10-07 21:00:55 -0300

    :sparkles: chore: animate header

--- src/components/Header/Gradient/index.tsx
+++ src/components/Header/Gradient/index.tsx
@@ -1,9 +1,41 @@
 import React, { memo } from 'react';
 
+import { useCycle } from 'framer-motion';
+import { useTheme } from 'styled-components';
+
 import { Container } from './styles';
 
+const animation = {
+  unMounted: { width: '0%' },
+  mounted: {
+    width: '100%',
+    transition: {
+      delay: 1,
+      duration: 2.5,
+      ease: 'easeInOut',
+    },
+  },
+};
+
 const Gradient: React.FC = () => {
-  return <Container />;
+  const { colors } = useTheme();
+  const [backgroud, cycleBackground] = useCycle(
+    {
+      backgroudColor: `linear-gradient(90deg, ${colors.secondary} 0%, ${colors.primary} 0%);`,
+    },
+    {
+      backgroudColor: `linear-gradient(90deg, ${colors.secondary} 0%, ${colors.primary} 100%);`,
+    },
+  );
+
+  return (
+    <Container
+      layout
+      variants={animation}
+      initial="unMounted"
+      animate="mounted"
+    />
+  );
 };
 
 export default memo(Gradient);
